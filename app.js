4a1b0efa1622b061fbb0b44963ce3521f28e59f4
ashutosh2407 <adwagh24@gmail.com>
2021-05-26 18:50:39 -0500

    created a variable for port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
 const sslRedirect = require('heroku-ssl-redirect').default
+var port = process.env.PORT || 3000;
 
 require('dotenv').config()
 
@@ -41,6 +42,6 @@ app.post("/",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port, function(){
   console.log("Server is running on port 3000");
 })
