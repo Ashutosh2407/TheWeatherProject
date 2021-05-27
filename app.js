a352939da5726b4cb5f6088035215eb48ee85356
ashutosh2407 <adwagh24@gmail.com>
2021-05-26 20:12:51 -0500

    added app.use(express.json());

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 require('dotenv').config()
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
